393242e73898e7d7298d8344bbbadb1e27562e04
Julia T <juliaturkali@gmail.com>
2025-04-09 11:24:24 +0200

    Enhance Footer component with social media links and improve layout structure

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
-
 const Footer = () => {
   return (
     <footer>
-      <div className="flex justify-between items-center bg-amber-300 px-4 py-8 ">
-        <h3 className="font-bold text-2xl">RecipeWorld</h3>
-        <div className="flex items-center gap-10 mx-4">
+      <div className="flex items-center justify-between bg-amber-300 px-4 py-8">
+        <h3 className="text-2xl font-bold">RecipeWorld</h3>
+        <div className="mx-4 flex items-center gap-10">
           <ul className="flex gap-4">
-            <li>Instagram</li>
-            <li>Facebook</li>
-            <li>X</li>
+            <li className="cursor-pointer">
+              <a href="https://instagram.com" target="_blank">
+                Instagram
+              </a>
+            </li>
+            <li className="cursor-pointer">
+              <a href="https://facebook.com" target="_blank">
+                Facebook
+              </a>
+            </li>
+            <li className="cursor-pointer">
+              <a href="https://x.com" target="_blank">
+                X
+              </a>
+            </li>
           </ul>
         </div>
       </div>
