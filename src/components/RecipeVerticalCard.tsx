5627687c5a871645e5c93902e00564ec4643934c
Julia T <juliaturkali@gmail.com>
2025-04-09 11:24:32 +0200

    Refactor RecipeVerticalCard component for improved code consistency and readability

--- src/components/RecipeVerticalCard.tsx
+++ src/components/RecipeVerticalCard.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import supabase from '../utils/supabase';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import supabase from "../utils/supabase";
+import { Link } from "react-router-dom";
 
 interface IRecipe {
   id: number;
@@ -13,7 +13,7 @@ const RecipeVerticalCard = () => {
   const [favoriteRecipes, setFavoriteRecipes] = useState<IRecipe[]>([]);
 
   const fetchData = async () => {
-    const { data: recipes } = await supabase.from('recipes').select('*');
+    const { data: recipes } = await supabase.from("recipes").select("*");
     if (recipes) {
       setFavoriteRecipes(recipes.slice(0, 3));
     }
@@ -24,22 +24,23 @@ const RecipeVerticalCard = () => {
   }, []);
 
   return (
-    <div className="favorite-recipes grid grid-cols-3 p-4 gap-4">
+    <div className="favorite-recipes grid grid-cols-3 gap-4 p-4">
       {favoriteRecipes.map((recipe) => (
         <div
           key={recipe.id}
-          className="recipe-card rounded-lg shadow-lg bg-white flex flex-col gap-2 h-80 justify-between">
+          className="recipe-card flex h-80 flex-col justify-between gap-2 rounded-lg bg-white shadow-lg"
+        >
           <img
             src={recipe.image_url}
             alt={recipe.name}
-            className="rounded-t-lg h-40 w-full object-cover"
+            className="h-40 w-full rounded-t-lg object-cover"
           />
           <article className="flex flex-col px-2 text-left">
-            <h3 className="font-bold mt-2 text-xl">{recipe.name}</h3>
+            <h3 className="mt-2 text-xl font-bold">{recipe.name}</h3>
             <p>{recipe.description}</p>
           </article>
-          <Link to={`/recipe/${recipe.id}`} className="px-2 ">
-            <button className="rounded-full bg-amber-300 px-4 py-2 font-sm mb-4">
+          <Link to={`/recipe/${recipe.name}`} className="px-2">
+            <button className="font-sm mb-4 cursor-pointer rounded-full bg-amber-300 px-4 py-2">
               View Recipe
             </button>
           </Link>
