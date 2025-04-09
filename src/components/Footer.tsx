7ca446e7ac3b0eea87e095aaa30d939b00c2f79a
Julia T <juliaturkali@gmail.com>
2025-04-09 13:41:14 +0200

    Make Footer title clickable to navigate to home page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,17 @@
+import { useNavigate } from "react-router-dom";
+
 const Footer = () => {
+  const navigate = useNavigate();
+
   return (
     <footer>
       <div className="flex items-center justify-between bg-amber-300 px-4 py-8">
-        <h3 className="text-2xl font-bold">RecipeWorld</h3>
+        <h3
+          className="cursor-pointer text-2xl font-bold"
+          onClick={() => navigate("/")}
+        >
+          RecipeWorld
+        </h3>
         <div className="mx-4 flex items-center gap-10">
           <ul className="flex gap-4">
             <li className="cursor-pointer">
