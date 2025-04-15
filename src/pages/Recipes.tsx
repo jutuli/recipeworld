732e2e2ad972d5e57228ed07240b450a42d7e67e
Julia T <juliaturkali@gmail.com>
2025-04-15 15:10:21 +0200

    Refactor Recipes component: conditionally render AddRecipe based on user authentication

--- src/pages/Recipes.tsx
+++ src/pages/Recipes.tsx
@@ -1,16 +1,22 @@
+import { useContext } from "react";
 import AddRecipe from "../components/AddRecipe";
 import FavoriteRecipes from "../components/FavoriteRecipes";
 import NewRecipes from "../components/NewRecipes";
+import { mainContext } from "../context/MainProvider";
 
 const Recipes = () => {
+  const { isLoggedIn } = useContext(mainContext) as { isLoggedIn: boolean };
+
   return (
     <div className="w-full">
       <FavoriteRecipes />
       <NewRecipes />
-      <div className="mb-10 flex flex-col items-center justify-center gap-2 font-semibold">
-        <p>Can't find, what you're looking for? Add your own recipe!</p>
-        <AddRecipe />
-      </div>
+      {isLoggedIn && (
+        <div className="mb-10 flex flex-col items-center justify-center gap-2 font-semibold">
+          <p>Can't find, what you're looking for? Add your own recipe!</p>
+          <AddRecipe />
+        </div>
+      )}
     </div>
   );
 };
