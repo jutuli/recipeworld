465d2f90dafa17e81ab9302fb447a60d84bef352
Julia T <juliaturkali@gmail.com>
2025-04-15 15:53:13 +0200

    Clean Imports

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useState } from "react";
 import { IUser } from "../interfaces/IUser";
 import { mainContext } from "../context/MainProvider";
 import { Link } from "react-router-dom";
